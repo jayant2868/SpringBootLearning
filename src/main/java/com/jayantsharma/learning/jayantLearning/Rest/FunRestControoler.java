d523cb009fda9ae6c9bec6906e663969a9d3b269
Jayant Babu <jayantbabu@microsoft.com>
2024-05-10 10:51:17 +0530

    Injecting custome application property using @Value annotation

--- src/main/java/com/jayantsharma/learning/jayantLearning/Rest/FunRestControoler.java
+++ src/main/java/com/jayantsharma/learning/jayantLearning/Rest/FunRestControoler.java
@@ -1,10 +1,27 @@
 package com.jayantsharma.learning.jayantLearning.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestControoler {
+
+    //inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo()
+    {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
 
     @GetMapping("/")
     public String sayHello()
