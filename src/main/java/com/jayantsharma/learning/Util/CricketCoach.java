adc041f0e55b35d4f514f737d55e818f57a9d153
Jayant Babu <jayantbabu@microsoft.com>
2024-05-31 23:27:14 +0530

    understanding Bean lifecycle methods

--- src/main/java/com/jayantsharma/learning/Util/CricketCoach.java
+++ src/main/java/com/jayantsharma/learning/Util/CricketCoach.java
@@ -4,12 +4,30 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.val;
+
 //@Componet Annotation marks the class as a Spring bean
 @Component
-// by default the bean scope will be singleton when not defined explicilty
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+
 public class CricketCoach implements Coach
 {
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff()
+    {
+        System.out.println("in doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMycleanupStuff()
+    {
+        System.out.println("in doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
     public  String getDailyWorkout()
     {
         return "Practice fast bowling for 15 minutes.";
--- src/main/java/com/jayantsharma/learning/jayantLearning/Rest/DemoBeanScopeController.java
+++ src/main/java/com/jayantsharma/learning/jayantLearning/Rest/DemoBeanScopeController.java
@@ -1,5 +1,22 @@
 package com.jayantsharma.learning.jayantLearning.Rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jayantsharma.learning.Util.Coach;
+
+@RestController
 public class DemoBeanScopeController {
 
+    private Coach myCoach1;
+
+    @Autowired
+    public DemoBeanScopeController(
+        @Qualifier("cricketCoach") Coach theCoach
+        )
+    {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+        myCoach1 = theCoach;
+    }
 }
